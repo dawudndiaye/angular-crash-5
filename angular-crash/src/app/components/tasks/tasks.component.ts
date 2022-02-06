4c1cd187227f9d49445434cfb1ed5f463dc7b7dc
LonelyBaguette <94087515+LonelyBaguette@users.noreply.github.com>
2022-02-06 15:40:39 +0800

    fourth commit

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -21,8 +21,14 @@ deleteTask(task: Task) {
   this.taskService
   .deleteTask(task)
   .subscribe(
-    () => (this.tasks = this.tasks.filter((t) => t.id! === task.id))
+    () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
     );
+}
+
+toggleReminder(task : Task) { 
+  task.reminder = !task.reminder;
+  this.taskService.updateTaskReminder(task).subscribe();
+
 }
 
 }
